272ebde833daed125947242cbfd7723e8b24349a
VitorLucasX <vitorlucas18@yahoo.com>
2024-07-07 17:50:44 -0300

    alteração em curriculo de seção about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,5 @@
 import './about.css';
-import CV from "../../assets/CurriculoVitorLucasAraujoPDF.pdf";
+import CV from "../../assets/CurriculoVitorLucasAraujo.pdf";
 import AboutImg from "../../assets/programming.png";
 import Info from './Info';
 import { IoCloudDownloadOutline } from 'react-icons/io5';
